7368767c23752fa7cd489a4c4bb9923bf5b51ce9
kevin <kevin@makdev.net>
2025-11-11 22:07:38 +0800

    update lora script

--- src/lora_expansion_board/LoRaReceiver/LoRaReceiver.ino
+++ src/lora_expansion_board/LoRaReceiver/LoRaReceiver.ino
@@ -1,8 +1,6 @@
 #include <SPI.h>
 #include <LoRa.h>
 
-//extern SPIClass SPI;
-
 void setup() {
   Serial.begin(115200);
  // while (!Serial);
@@ -11,10 +9,17 @@ void setup() {
   //LoRa.setSPI(SPI);
   LoRa.setPins(10,9,5);
 
+
+
   if (!LoRa.begin(433E6)) {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
+  LoRa.setSpreadingFactor(7);     // 設置擴頻因子
+  LoRa.setSignalBandwidth(125E3); // 設置信號帶寬 
+  LoRa.setCodingRate4(5);         // 設置編碼率  
+  LoRa.setSyncWord(0x12);          // 同步訊號字，需與傳送端一致
+  LoRa.setTxPower(10);   
 }
 
 void loop() {
--- src/lora_expansion_board/LoRaSender/LoRaSender.ino
+++ src/lora_expansion_board/LoRaSender/LoRaSender.ino
@@ -9,15 +9,20 @@ void setup() {
 
   Serial.println("LoRa Sender");
 
-  //LoRa.setSPI(SPI);
+
   LoRa.setPins(10,9,5);
-  //LoRa.setSPIFrequency(2000000);
+
  
 
   if (!LoRa.begin(433E6)) {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
+  LoRa.setSpreadingFactor(7);     // 設置擴頻因子
+  LoRa.setSignalBandwidth(125E3); // 設置信號帶寬 
+  LoRa.setCodingRate4(5);         // 設置編碼率  
+  LoRa.setSyncWord(0x12);          // 同步訊號字，需與傳送端一致
+  LoRa.setTxPower(10);    
 
 }
 
